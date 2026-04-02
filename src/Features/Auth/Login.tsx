ff9e2dc0eae549c7c732f0092870abfd8973e393
barez azad <barez.001159544@gmail.com>
2026-04-02 03:40:47 +0300

    Login page design is good

--- src/Features/Auth/Login.tsx
+++ src/Features/Auth/Login.tsx
@@ -1,4 +1,5 @@
 import checkIcon from "../../assets/icons/checked.png";
+import rightArrowIcon from "../../assets/icons/right-arrow.png";
 import "./Components/Login.css";
 
 export default function Login() {
@@ -11,7 +12,7 @@ export default function Login() {
             with Notefy app
           </h2>
           <p>
-            The most trus worthy collaborator to save your <br /> daily notes,
+            The most trust worthy collaborator to save your <br /> daily notes,
             and make memroies.
           </p>
           <div className="motos">
@@ -21,11 +22,11 @@ export default function Login() {
             </div>
             <div className="moto-2">
               <img src={checkIcon} alt="Check Icon" />
-              <h6>Safe & Secure</h6>
+              <h6>Fast & Reliable</h6>
             </div>
             <div className="moto-3">
               <img src={checkIcon} alt="Check Icon" />
-              <h6>Safe & Secure</h6>
+              <h6>Comprehensive Data</h6>
             </div>
           </div>
         </div>
@@ -33,9 +34,27 @@ export default function Login() {
         <div className="right-component">
           <h6>Welcome back!</h6>
           <h4>Login to your account</h4>
-          <input type="text" className="username-field" />
-          <input type="password" className="password-field" />
-          <button className="continue-button">Continue</button>
+          <input
+            type="text"
+            className="username-field"
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            className="password-field"
+            placeholder="Password"
+          />
+          <button className="continue-button">
+            <h2>Continue</h2>
+            <img src={rightArrowIcon} alt="Arrow" />
+          </button>
+
+          <div className="divider"></div>
+
+          <button className="other-button">
+            <h2>Create Account</h2>
+            <img src={rightArrowIcon} alt="Arrow" />
+          </button>
         </div>
       </div>
     </section>
