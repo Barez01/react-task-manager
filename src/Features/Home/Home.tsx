a4f1677be6e590e2c727e991aa0b8074e1db0dba
barez azad <barez.001159544@gmail.com>
2026-04-08 01:52:10 +0300

    Git hub box added

--- src/Features/Home/Home.tsx
+++ src/Features/Home/Home.tsx
@@ -1,5 +1,6 @@
 import "./Components/Home.css";
 import botImage from "../../assets/images/bot.png";
+import githubLogo from "../../assets/images/github-logo.png";
 import { ArrowIcon } from "../../Constants/Icons/arrow_icon";
 
 export default function Home() {
@@ -9,7 +10,10 @@ export default function Home() {
         <div className="parent">
           <div className="div1 animated-border">
             <img src={botImage} alt="" />
-            <h4>Hello, what is on your <br/>mind today?</h4>
+            <h4>
+              Hello, what is on your <br />
+              mind today?
+            </h4>
             <div className="note-form">
               {/* <input
             type="text"
@@ -18,25 +22,31 @@ export default function Home() {
             // value={username}
             // onChange={(e) => setUsername(e.target.value)}
           /> */}
-            <input
-            type="text"
-            className="description-field"
-            placeholder="Type something..."
-            // value={username}
-            // onChange={(e) => setUsername(e.target.value)}
-          />
-          <button className="note-submit-button">
-                      {/* <h2>{false ? "Loading..." : "Continue"}</h2> */}
-                      {false ? (
-                        <div className="loader"></div>
-                      ) : (
-                        ArrowIcon({ color: "#fff", size: 32 })
-                      )}
-                    </button>
+              <input
+                type="text"
+                className="description-field"
+                placeholder="Type something..."
+                // value={username}
+                // onChange={(e) => setUsername(e.target.value)}
+              />
+              <button className="note-submit-button">
+                {/* <h2>{false ? "Loading..." : "Continue"}</h2> */}
+                {false ? (
+                  <div className="loader"></div>
+                ) : (
+                  ArrowIcon({ color: "#fff", size: 32 })
+                )}
+              </button>
             </div>
           </div>
           <div className="div2"></div>
-          <div className="div3"></div>
+          <div className="div3">
+            <h1>Follow me</h1>
+            <p>If you find this project usefull, <br/>you can follow me</p>
+            <div className="image-container">
+              <img src={githubLogo} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
