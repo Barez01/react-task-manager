6cee47ab52653fcbd841d2efe31d764e5daf4cba
barez azad <barez.001159544@gmail.com>
2026-04-08 01:26:52 +0300

    Home note box is good

--- src/Features/Home/Home.tsx
+++ src/Features/Home/Home.tsx
@@ -1,13 +1,42 @@
 import "./Components/Home.css";
+import botImage from "../../assets/images/bot.png";
+import { ArrowIcon } from "../../Constants/Icons/arrow_icon";
 
 export default function Home() {
   return (
     <section className="home">
       <div className="blur-component">
         <div className="parent">
-          <div className="div1 animated-border">1</div>
-          <div className="div2">2</div>
-          <div className="div3">3</div>
+          <div className="div1 animated-border">
+            <img src={botImage} alt="" />
+            <h4>Hello, what is on your <br/>mind today?</h4>
+            <div className="note-form">
+              {/* <input
+            type="text"
+            className="title-field"
+            placeholder="Title here..."
+            // value={username}
+            // onChange={(e) => setUsername(e.target.value)}
+          /> */}
+            <input
+            type="text"
+            className="description-field"
+            placeholder="Type something..."
+            // value={username}
+            // onChange={(e) => setUsername(e.target.value)}
+          />
+          <button className="note-submit-button">
+                      {/* <h2>{false ? "Loading..." : "Continue"}</h2> */}
+                      {false ? (
+                        <div className="loader"></div>
+                      ) : (
+                        ArrowIcon({ color: "#fff", size: 32 })
+                      )}
+                    </button>
+            </div>
+          </div>
+          <div className="div2"></div>
+          <div className="div3"></div>
         </div>
       </div>
     </section>
