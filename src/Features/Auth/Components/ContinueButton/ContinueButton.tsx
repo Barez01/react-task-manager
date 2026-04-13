2bf75f989753bee5b7ce08c8424654ee418aa3f9
barez azad <barez.001159544@gmail.com>
2026-04-14 02:50:55 +0300

    Auth Buttons css created

--- src/Features/Auth/Components/ContinueButton/ContinueButton.tsx
+++ src/Features/Auth/Components/ContinueButton/ContinueButton.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { ArrowIcon } from "../../../../Constants/Icons/arrow_icon";
+import "./ContinueButton.css";
 
 export default function ContinueButton({
   loading,
